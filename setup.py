12d1c7b9ba489586c2cad8cfdb5fd8641cdd5b9d
saisubhasish <saisubhasishrout777@gmail.com>
2023-08-30 13:14:38 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     description="A small python package",
     long_description=long_decription,
     long_decription_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}"
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
